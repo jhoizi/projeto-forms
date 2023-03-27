3a479cf90e43ee3320a4bb547a686bed982afa4c
Jhônatan Izidro Almeida <77496500+jhoizi@users.noreply.github.com>
2023-03-27 16:53:55 -0300

    Update db.js

--- back-end/db.js
+++ back-end/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function startDB(){
         try{
-            mongoose = await mongoose.connect(process.env.MONGODB_URL).then(() => {console.log('conectado...')}).catch((err) => {console.log(err)});
+            await mongoose.connect(process.env.MONGODB_URL).then(() => {console.log('conectado...')}).catch((err) => {console.log(err)});
         }catch(err){
                 console.log(err);
         }      
