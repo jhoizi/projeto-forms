2db46e24886608fd04055df51043356134e40e90
Jhônatan Izidro Almeida <77496500+jhoizi@users.noreply.github.com>
2023-03-28 13:19:50 -0300

    Update AuthorizationController.js

--- back-end/controllers/AuthorizationController.js
+++ back-end/controllers/AuthorizationController.js
@@ -17,7 +17,7 @@ class AuthorizationController{
         }
 
         try{
-            const decode = await promisify(jwt.verify(token, chave));
+            const decode = await promisify(jwt.verify)(token, chave);
             return next();
         }catch(err){
             return res.status(400).json({messagem : `Erro ao decodificar token! ${err}`});
